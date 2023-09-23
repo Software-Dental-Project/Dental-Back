1f0b4daf177834fd0a042b562e7b9b06eeb78070
JoseR200 <joserodrigolopez@icloud.com>
2023-09-24 00:16:43 +0200

    added new endpoint for agenda

--- controllers/agendaController.js
+++ controllers/agendaController.js
@@ -53,6 +53,50 @@ const getAgendaBySede = async (req, res) => {
     });
 }
 
+const getHistoryBySede = async (req, res) => {
+    let userId = new ObjectId(req.user.id);
+    let agenda = [];
+
+    let consultations = await Consultation.find().populate([{ path: "patient", populate: { path: "personData" } }, { path: "doctor", populate: { path: "personData" } }, { path: "campus", populate: { path: "user", match: { _id: userId } } }]).sort('date hour');
+    consultations.forEach(consultation => {
+        if (consultation.campus.user) {
+            agenda.push({ 
+                fullNameDoctor: consultation.doctor.personData.names + ' ' + consultation.doctor.personData.fatherLastName + ' ' + consultation.doctor.personData.motherLastName,
+                fullNamePatient: consultation.patient.personData.names + ' ' + consultation.patient.personData.fatherLastName + ' ' + consultation.patient.personData.motherLastName,
+                hour: consultation.hour,
+                typeEvent: "Consulta",
+                _id: consultation._id
+            });
+        }
+    });
+
+    let treatmentAppointments = await TreatmentAppointment.find().populate([{ path: "doctor", populate: { path: "personData" } }, { path: "campus", populate: { path: "user", match: { _id: userId } } }, { path: "treatmentDetail", populate: { path: "patient", populate: { path: "personData" } } } ]).sort('date hour');
+    treatmentAppointments.forEach(treatmentAppointment => {
+        if (treatmentAppointment.campus.user) {
+            agenda.push({
+                fullNameDoctor: treatmentAppointment.doctor.personData.names + ' ' + treatmentAppointment.doctor.personData.fatherLastName + ' ' + treatmentAppointment.doctor.personData.motherLastName,
+                fullNamePatient: treatmentAppointment.treatmentDetail.patient.personData.names + ' ' + treatmentAppointment.treatmentDetail.patient.personData.fatherLastName + ' ' + treatmentAppointment.treatmentDetail.patient.personData.motherLastName,
+                hour: treatmentAppointment.hour,
+                typeEvent: "Cita",
+                _id: treatmentAppointment._id
+            })
+        }
+    });
+
+    if (agenda.length == 0) {
+        return res.status(404).json({
+            status: "Error",
+            message: "Citas y consultas no encontradas en agenda para el dia de hoy"
+        });
+    }
+
+    return res.status(200).json({
+        "status": "success",
+        agenda
+    });
+}
+
 module.exports = {
-    getAgendaBySede
+    getAgendaBySede,
+    getHistoryBySede
 }
--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -5,5 +5,6 @@ const AgendaController = require("../controllers/agendaController");
 const check = require("../authorization/auth");
 
 router.get("/", check.auth, AgendaController.getAgendaBySede);
+router.get("/history", check.auth, AgendaController.getHistoryBySede);
 
 module.exports = router;
